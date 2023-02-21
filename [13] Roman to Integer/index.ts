41f79b687de08c998fd6464f42c19c0759de383f
TakuyaShinoda <takuuuya.s6114@gmail.com>
2023-02-21 19:32:01 +0900

    [13] Roman to Integer

---
+++ [13] Roman to Integer/index.ts
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/roman-to-integer/description/
+
+function romanToInt(s: string): number {
+  const symbol = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+
+  let result = 0
+
+  for(let i = 0; i < s.length; i++) {
+    const curr = symbol[s[i]]
+    const next = symbol[s[i + 1]]
+    curr < next ? result -= curr : result += curr
+  }
+
+  return result
+};
